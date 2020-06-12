83474180cf1a2e3a96d1372bcce8edd9a550cc51
wnbuitragoc <wnbuitragoc@correo.udistrital.edu.co>
2020-06-12 17:10:37 -0500

    Ya encuentras el menor y ubica la etiqueta en lista final

--- Camino casi más corto.py
+++ Camino casi más corto.py
@@ -1,5 +1,5 @@
 while True:
-	#Asignación de los valores de n y m. Creación de la lista de vértices
+	#Asignación de los valores de n y m. Creación de la lista de vértices, temporal y final
 	entrada=input("Ingrese N y M: ")
 	if entrada=="0 0":
 		break
@@ -22,19 +22,16 @@ while True:
 			if(i!=" "):
 				m=m+i
 
-	n= int(n)
+	n= int(n)	#Conversión a enteros de N y M
 	m= int(m)
-	#En este for llenamos la listaVertices con los valores 1...(n-1)
 
-	for i in range(n):
+	for i in range(n):#En este for llenamos la listaVertices con los valores 1...(n-1)
 		listaVertices.append(i)
 
 	 
-	print(n,m)
+	#print(n,m)  Imprime los valores de n y m
 
-	#Asignación de los puntos de inicio y destino
-
-	entrada1=input("Ingrese S y D: ")
+	entrada1=input("Ingrese S y D: ")#Asignación de los puntos de inicio y destino
 
 	s=""
 	d=""
@@ -50,22 +47,23 @@ while True:
 			if(i!=" "):
 				d=d+i
 
-	s= int(s)
+	s= int(s)#Conversión a enteros de S y D
 	d= int(d)
 
-	print(s,d)
+	#print(s,d) Imprime S y D
 
-	for i in range(n-1):
+	for i in range(n-1):#Iniciliza las listas Temporal y Final con ceros
 		listaTemporal.append(0)
 		listaFinal.append(0)
 
 	listaTemporal.insert(s,0)
-	listaFinal.insert(s,0)
+	listaFinal.insert(s,0)#En las posiciones S de las listas temporal y final pone un cero
 
-	print(listaTemporal[:])
-	print(listaFinal[:])
+	#print(listaTemporal[:])
+	#print(listaFinal[:]) Imprime las listas temporal y final con sólo los ceros en la posición S
 
-	#Asignación de los caminos establecidos
+	############### Asignación de las aristas #####################################
+
 	diccionarioAristas={}
 	for i in range(m):
 		u=""
@@ -88,17 +86,22 @@ while True:
 			else :
 				p=p+i
 		
-		diccionarioAristas[u+" "+v]=int(p)
+		diccionarioAristas[u+" "+v]=int(p)#Al diccionario de aristas le da una clave de "u v" y un valor entero p
 
-	#Algoritmo#################################
+	############### Fin de asignación de las aristas #####################################
+
+
+	##################### Inicio algoritmo #################################
+
 	verticeActivo=str(s)
 	diccionarioAdyacentes={}
 	aux=""
 	contador=0
 	aux2=""
+	menor=0
+	indexMenor=None
 
-
-	print(diccionarioAristas.keys())
+	#print(diccionarioAristas.keys()) Imprime las claves del diccionarioAristas
 	
 	#Mira si un vértice es adyacente
 	for i in diccionarioAristas.keys():	
@@ -118,37 +121,43 @@ while True:
 						aux2=aux2+k
 				diccionarioAdyacentes[aux2]=diccionarioAristas[i]
 				listaTemporal[int(aux2)]=diccionarioAristas[i]
+				if menor==0:
+					menor=diccionarioAristas[i]
+					indexMenor=int(aux2)
+				else:
+					if diccionarioAristas[i]<menor:
+						menor=diccionarioAristas[i]
+						indexMenor=int(aux2)
 				print(i + " Es un vértice adyacente")
 				break
 			else:
 				print(i+ " no es adyacente")
 				break
 
-	menor=0
-	for i in range(n-1):
-		if listaTemporal[i]>0:
+	print("El índice es: " + str(indexMenor))
+	print("El menor es: "+ str(menor))
 			
-
-	print(menor)
+	listaFinal[indexMenor]=menor
+	
 
 
 	
 	print(diccionarioAdyacentes)
 	print(listaTemporal[:])	
+	print(listaFinal[:])
 
 
 
 
 
-	#Algoritmo#################################
+	#####################Fin algoritmo #################################
 
 
 
 
-print(diccionarioAristas)
+#print(diccionarioAristas) Imprime todas las aristas del grafo
 
 
-	
 	
 
 
